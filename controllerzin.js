c00e32cb833983b7fbd95392e9aaca690e68b29f
Paulo Henrique Fernandes De Souza <86264171+Takitsu-dev@users.noreply.github.com>
2021-09-02 16:05:28 -0300

    Create controllerzin.js

---
+++ controllerzin.js
@@ -0,0 +1,19 @@
+function entrar(){
+
+    var usuario = document.getElementById("loginEmail").value;
+
+    var senha = document.getElementById("loginSenha").value;
+
+
+
+    if(!usuario || !senha){
+
+        alert("Preencha os campos obrigatórios!");
+
+    }else{
+
+        window.open = "https://www.youtube.com/";
+
+    }
+
+}
